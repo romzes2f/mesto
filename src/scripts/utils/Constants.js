ea6ed1d8d7ec80b9b180c5f40a9008679a476e90
romzes2f <roman.v.shchebet@gmail.com>
2023-06-16 16:40:31 +0200

    Code fixes

--- src/scripts/utils/Constants.js
+++ src/scripts/utils/Constants.js
@@ -1,30 +1,30 @@
-///Константы профиля////
+///Profile constants////
 export const userNameSelector = '.profile__name';
 export const userStatusSelector = '.profile__description';
 export const userAvatarSelector = '.profile__avatar';
 
-///Константы попапа профиля////
+///Profile popup constants////
 export const popupProfileSelector = '.popup-profile';
 export const popupOpenButtonProfile = document.querySelector(".profile__edit-button");
 export const popupFormProfile = document.querySelector(".popup-profile__form");
 export const popupProfileInputName = popupFormProfile.querySelector('.popup__input_type_name');
 export const popupProfileInputStatus = popupFormProfile.querySelector('.popup__input_type_description');
 
-///Константы попапа карточки////
+///Place popup constants////
 export const popupPlaceSelector = '.popup-place';
 export const popupOpenButtonPlace = document.querySelector(".profile__add-button");
 export const popupFormPlace = document.querySelector(".popup-place__form");
 export const popupPlaceInputName = popupFormPlace.querySelector('.popup__input_type_place');
 export const popupPlaceInputLink = popupFormPlace.querySelector('.popup__input_type_link');
 
-///Константы попапа аватара////
+///Avatar constants////
 export const popupFormAvatar = document.querySelector(".popup-avatar__form");
 export const popupAvatarSelector = '.popup-avatar';
 export const popupOpenButtonAvatar = document.querySelector(".profile__avatar-edit");
 export const avatarUrl = document.querySelector(".profile__avatar");
 export const popupAvatarInput = document.querySelector(".popup__input_type_avatar");
 
-///Константы валидации////
+///Validation constants///
 export const validationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
@@ -34,7 +34,7 @@ export const validationConfig = {
     errorClass: 'popup__error_active',    
 }
 
-///Константы карточек////
+///Cards constants////
 export const cardListSelector = '.place-container';
 export const templateSelector = '.element-template';
 export const popupImageSelector = '.popup-image';
